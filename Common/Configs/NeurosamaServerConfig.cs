e899b5eb43a18a56bf02572f9ce902be9c2dc118
MongusaEye <186921147+MongusaEye@users.noreply.github.com>
2026-07-07 09:41:59 -0700

    fix server config formatting

--- Common/Configs/NeurosamaServerConfig.cs
+++ Common/Configs/NeurosamaServerConfig.cs
@@ -3,11 +3,9 @@ using Terraria.ModLoader.Config;
 
 namespace Neurosama.Common.Configs
 {
-
     public class NeurosamaServerConfig : ModConfig
     {
         // ConfigScope.ServerSide should be used for server side, usually gameplay-related tweaks.
-
         public override ConfigScope Mode => ConfigScope.ServerSide;
 
         [Header("Ermphibians")]
